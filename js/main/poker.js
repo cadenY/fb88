9689a0eb56186c6ccc80318757ce5876bad61b70
Gerry Asuncion <gerry.asia64@gmail.com>
2017-04-19 00:57:38 +0800

    challenge panel done

--- js/main/poker.js
+++ js/main/poker.js
@@ -1,14 +1,16 @@
 define(['jquery','fun','init','aos','owlCarousel'],function($,fun,init,AOS,owlCarousel) {
-//	AOS.init({
-//		easing: 'ease-out-back',
-//		duration: 1500,
-//		once: true
-//	});
-   $('.owl-carousel').owlCarousel({
-      loop:true,
-      items:1,
-      nav: true,
-      mouseDrag: false
-   })
-
+	AOS.init({
+		easing: 'ease-out-back',
+		duration: 1500,
+		once: true
+	});
+   setTimeout(function() {
+      $('.owl-carousel').owlCarousel({
+         loop:true,
+         items:1,
+         nav: true,
+         mouseDrag: false
+      });  
+   },300);
+   
 });
