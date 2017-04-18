470cc1caa778aff00bec374edff2394aa7508224
Gerry Asuncion <gerry.asia64@gmail.com>
2017-04-19 00:46:15 +0800

    inital lottery commit

---
+++ js/config.js
@@ -0,0 +1,40 @@
+requirejs.config({
+   packages: [
+      {
+        name: 'moment',
+        location: 'bower_components/moment',
+        main: 'moment'
+      },
+      {
+         name: 'moment-timezone',
+         location: 'bower_components/moment-timezone',
+         main: 'moment-timezone'
+      },
+      {
+         name: 'aos',
+         location: 'bower_components/aos/dist',
+         main: 'aos'
+      },
+      {
+         name: 'fancybox',
+         location: 'bower_components/fancybox/dist',
+         main: 'jquery.fancybox'
+      }
+   ],
+   baseUrl: 'js',
+   paths: {
+      'jquery': 'lib/jquery.min',
+      'fun': 'method/functions',
+      'init': 'method/init'
+   },
+   shim: {
+      'fun': {
+         deps: ["jquery"],
+         exports: "fun"
+      },
+      'init': {
+         deps: ["jquery","fun"],
+         exports: "init"
+      }
+   }
+})
