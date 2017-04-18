3f43cb89b0c0f14a0f6d3bce50ab86eb1830225b
Gerry Asuncion <gerry.asia64@gmail.com>
2017-04-19 00:34:30 +0800

    prodlink click event

--- js/main/casino.js
+++ js/main/casino.js
@@ -13,5 +13,18 @@ define(['jquery','fun','init','aos','owlCarousel'],function($,fun,init,AOS,owlCa
 //         mouseDrag: false,
 //      });
 //   },300);
+    
+    $('.prodlink .platform img').click(function() {
+        var id = $(this).data('id');
+        
+        $('#'+id+'.single-content').addClass('active');
+        $('.platform-content').delay(100).fadeIn(300);
+    });
+    
+    $('.pc-close').click(function() {
+        $('.platform-content').fadeOut(300,function() {
+            $('.single-content').removeClass('active');
+        });
+    });
 
 });
