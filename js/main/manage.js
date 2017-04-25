3367910577d7e2a2f25a94d7a46c48404d23a9d4
Gerry Asuncion <gerry.asia64@gmail.com>
2017-04-25 20:56:46 +0800

    file updates

--- js/main/manage.js
+++ js/main/manage.js
@@ -1,3 +1,11 @@
 $(function(){
-    
+    // on page load
+    setIframeSrc('account');
+
+    function setIframeSrc(url) {
+        $('#frame').attr('src','manage/'+url+'.html');
+        $('#frame').iFrameResize({
+            minHeight : 270
+        });
+    }
 });
