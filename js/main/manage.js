fcaa5694bac4e2e60f2336926526c202172ead29
Gerry Asuncion <gerry.asia64@gmail.com>
2017-04-25 20:17:38 +0800

    account editor link onclick event added

--- js/main/manage.js
+++ js/main/manage.js
@@ -2,6 +2,13 @@ $(function(){
     // on page load
     setIframeSrc('account');
 
+    // click event
+    $('.acct-mngr-nav ul li a').click(function(e) {
+        $('.acct-mngr-nav ul li').removeClass('active');
+        $(this).parent().addClass('active');
+        setIframeSrc($(this).data('href'));
+    });
+
     function setIframeSrc(url) {
         $('#frame').attr('src','manage/'+url+'.html');
         $('#frame').iFrameResize({
