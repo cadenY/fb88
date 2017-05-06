0f2d1a8f430f1dadd949992057f8f4159d8b67cb
gryinc <gerryasuncion@gmail.com>
2017-05-07 05:24:19 +0800

    payment page initially done | to follow error message on input fields

--- js/main/payment.js
+++ js/main/payment.js
@@ -1,18 +1,51 @@
 $(function(){
     // on page load
-    setIframeSrc('account');
-
-    // click event
-    $('.acct-mngr-nav ul li a').click(function(e) {
-        $('.acct-mngr-nav ul li').removeClass('active');
-        $(this).parent().addClass('active');
-        setIframeSrc($(this).data('href'));
+    $('iframe').iFrameResize({
+        minHeight : 0
     });
 
-    function setIframeSrc(url) {
-        $('#frame').attr('src','manage/'+url+'.html');
-        $('#frame').iFrameResize({
-            minHeight : 270
-        });
+    setIframeSrc('account',0);
+    setIframeSrc('transfer',1);
+
+    // show hide account setting panel
+    $('.acct-profile img').click(function(e){
+        e.preventDefault();
+        if(!$(this).parent().hasClass('active')){
+            $('.account-mngr').animate({
+                height: 269
+            },600);
+            $(this).parent().addClass('active');
+        }else {
+            $('.account-mngr').css('overflow','hidden');
+            $('.account-mngr').animate({
+                height: 0
+            },600);
+            $(this).parent().removeClass('active');
+        }
+    });
+
+    // click event for manage account nav
+    $('.acct-mngr-nav ul li a').click(function(e) {
+        $('.account-mngr').css('height','auto');
+
+        $('.acct-mngr-nav ul li').removeClass('active');
+        $(this).parent().addClass('active');
+        setIframeSrc($(this).data('href'),0);
+    });
+
+    $('.mgnt-deposit-nav ul li a').click(function(e) {
+        $('.mgnt-deposit-nav ul li').removeClass('active');
+        $(this).parent().addClass('active');
+        setIframeSrc($(this).data('href'),1);
+    });
+
+    function setIframeSrc(url,ref) {
+        switch(ref){
+            case 0:
+                $('#frame').attr('src','manage/'+url+'.html');
+                break;
+            default:
+                $('#depoFrame').attr('src','deposit/'+url+'.html');
+        }
     }
 });
