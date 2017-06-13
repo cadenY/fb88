1740c2fc5326fae6c1f984369dd164174ff1b53d
gryinc <gerryasuncion@gmail.com>
2017-06-13 23:40:15 +0800

    added url checker

--- js/main/sidebar.js
+++ js/main/sidebar.js
@@ -1,9 +1,12 @@
 var jq = jQuery.noConflict();
 
 jq(document).ready(function() {
+   var url = window.location.href.toString(),
+       str = url.indexOf("/payment/"), prefix = '';
+       (str != -1 ? prefix = '../' : prefix = '');
    var sidebar = '<div id="btn-download">'
       +'<div class="slidingDiv">'
-           +'<div class="slideIconTop"><img src="images/casino/leftpush/top.png"></div>'
+           +'<div class="slideIconTop"><img src="'+prefix+'images/casino/leftpush/top.png"></div>'
            +'<h3><b>DOWNLOADS</b></h3> <span>PC Version</span>'
            +'<div class="cbp-spmenu cbp-spmenu-vertical">'
                +'<a href="#">'
@@ -69,23 +72,23 @@ jq(document).ready(function() {
                +'</a>'
            +'</div>'
       +'</div>'
-      +'<div id="btnCloseDownload"> <img src="images/casino/leftpush/close-button.png" alt=""></div>'
+      +'<div id="btnCloseDownload"> <img src="'+prefix+'images/casino/leftpush/close-button.png" alt=""></div>'
    +'</div>'
    +'<div id="btn-Chat">'
-      +'<div class="imgCustomerServ"><img src="images/casino/girl-chat.png"></div>'
-      +'<div class="tcenter bottommargin-sm"><img src="images/casino/24-7.png"></div>'
+      +'<div class="imgCustomerServ"><img src="'+prefix+'images/casino/girl-chat.png"></div>'
+      +'<div class="tcenter bottommargin-sm"><img src="'+prefix+'images/casino/24-7.png"></div>'
       +'<div class="waysto clearfix">'
-           +'<div class="col_half nomargin tcenter"> <img src="images/casino/deposit-icon.png" />'
+           +'<div class="col_half nomargin tcenter"> <img src="'+prefix+'images/casino/deposit-icon.png" />'
                +'<br>'
                +'<div class="">How to <br> Deposit</div>'
            +'</div>'
-           +'<div class="col_half col_last nomargin tcenter"> <img src="images/casino/withdraw-icon.png" />'
+           +'<div class="col_half col_last nomargin tcenter"> <img src="'+prefix+'images/casino/withdraw-icon.png" />'
                +'<br>'
                +'<div class="">How to <br> Withdraw</div>'
            +'</div>'
       +'</div>'
       +'<div class="custService tcenter"> <a href="#"><span class="custIcon mail-icon"></span> help@fabet.com</a> <a href="#"><span class="custIcon phone-icon"></span> +852 8888 7168 <br> <span class="secondtext">International Call Charges May Apply</span></a> <a href="#"><span class="custIcon phone2-icon"></span> +852 8888 7168</a> <a href="#"><span class="custIcon line-icon"></span> +852 8888 7168</a> <a href="#"><span class="custIcon zolo-icon"></span> +852 8888 7168</a></div>'
-      +'<p id="btnCloseChat"><img src="images/casino/leftpush/help-center.png" alt=""></p>'
+      +'<p id="btnCloseChat"><img src="'+prefix+'images/casino/leftpush/help-center.png" alt=""></p>'
    +'</div>';
 
    jq('body').append(sidebar);
